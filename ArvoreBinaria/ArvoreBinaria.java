9512d2dfffbd81511b0f44aa64ecb178f3ba7081
leort11 <leonardo.figueiredo31@hotmail.com>
2024-05-21 12:45:43 -0300

    Adicionando o metodo remover() na classe arvore binaria

--- ArvoreBinaria/ArvoreBinaria.java
+++ ArvoreBinaria/ArvoreBinaria.java
@@ -4,6 +4,7 @@ public class ArvoreBinaria<X extends Comparable> {
 
     public ArvoreBinaria() {
         this.raiz = null;
+
     }
 
     // Adiciona um valor na árvore:
@@ -16,20 +17,25 @@ public class ArvoreBinaria<X extends Comparable> {
 
         } else { // Se a árvore não estiver vazia, percorre a árvore para encontrar a posição correta para o novo nó:
             No<X> atual = this.raiz;
+
             while(true) {
                 if (novoNo.getValor().compareTo(atual.getValor()) == -1) {
                     if (atual.getEsquerda() != null) {
                         atual = atual.getEsquerda();
+
                     } else {
                         atual.setEsquerda(novoNo);
                         break;
+
                     }
                 } else { // se for maior ou igual
                     if (atual.getDireita() != null) {
                         atual = atual.getDireita();
+
                     } else {
                         atual.setDireita(novoNo);
                         break;
+
                     }
                 }
             }
@@ -38,6 +44,7 @@ public class ArvoreBinaria<X extends Comparable> {
 
     public No<X> getRaiz() {
         return raiz;
+
     }
 
     // Percorre a árvore em ordem:
@@ -46,6 +53,7 @@ public class ArvoreBinaria<X extends Comparable> {
             emOrdem(atual.getEsquerda());
             System.out.println(atual.getValor());
             emOrdem(atual.getDireita());
+
         }
     }
 
@@ -55,6 +63,7 @@ public class ArvoreBinaria<X extends Comparable> {
             System.out.println(atual.getValor());
             preOrdem(atual.getEsquerda());
             preOrdem(atual.getDireita());
+
         }
     }
 
@@ -64,6 +73,114 @@ public class ArvoreBinaria<X extends Comparable> {
             posOrdem(atual.getEsquerda());
             posOrdem(atual.getDireita());
             System.out.println(atual.getValor());
+
+        }
+    }
+
+    // Remove um valor da árvore:
+    public boolean remover(X valor) {
+        // buscar o elemento na arvore
+        No<X> atual = this.raiz;
+        No<X> paiAtual = null;
+
+        while(atual != null) {
+            if (atual.getValor().equals(valor)) { // O valor procurado é igual ao atual.
+                break;
+
+            } else if (valor.compareTo(atual.getValor()) == -1){ // O valor é menor que o atual, então ele vai para a esquerda.
+                paiAtual = atual;
+                atual = atual.getEsquerda();
+
+            } else {
+                paiAtual = atual; // O valor não é igual nem menor, então é maior, e anda para a direita.
+                atual = atual.getDireita();
+
+            }
+        }
+
+        // Verifica se existe o elemento.
+        if (atual != null) {
+
+            // No tem 2 filhos ou No tem somente filho a direita.
+            if (atual.getDireita() != null) {
+                No<X> substituto = atual.getDireita();
+                No<X> paiSubstituto = atual;
+
+                while(substituto.getEsquerda() != null) {
+                    paiSubstituto = substituto;
+                    substituto = substituto.getEsquerda();
+                    
+                }
+                substituto.setEsquerda(atual.getEsquerda());
+                if (paiAtual != null){
+
+                    if (atual.getValor().compareTo(paiAtual.getValor())  == -1) { // atual < paiAtual
+                        paiAtual.setEsquerda(substituto);
+
+                    } else {
+                        paiAtual.setDireita(substituto);
+
+                    }
+
+                } else { // Se não tem paiAtual, então é a raiz.
+                    this.raiz = substituto;
+
+                }
+
+                // Removeu o elemento da arvore.
+                if (substituto.getValor().compareTo(paiSubstituto.getValor())  == -1) { // substituto < paiSubstituto
+                    paiSubstituto.setEsquerda(null);
+                } else {
+                    paiSubstituto.setDireita(null);
+                }
+
+            } else if (atual.getEsquerda() != null) { // Tem filho só à esquerda.
+                No<X> substituto = atual.getEsquerda();
+                No<X> paiSubstituto = atual;
+
+                if (paiAtual != null) {
+                    while(substituto.getDireita() != null) {
+                        paiSubstituto = substituto;
+                        substituto = substituto.getDireita();
+                        
+                    }
+                } else { // Se for a raiz.
+                    this.raiz = substituto;
+
+                }
+
+                if (atual.getValor().compareTo(paiAtual.getValor())  == -1) { // atual < paiAtual
+                    paiAtual.setEsquerda(substituto);
+
+                } else {
+                    paiAtual.setDireita(substituto);
+
+                }
+
+                // Removeu o elemento da arvore.
+                if (substituto.getValor().compareTo(paiSubstituto.getValor())  == -1) { // substituto < paiSubstituto
+                    paiSubstituto.setEsquerda(null);
+                } else {
+                    paiSubstituto.setDireita(null);
+                }
+
+            } else { // Não tem filho.
+                if (paiAtual != null) {
+                    if (atual.getValor().compareTo(paiAtual.getValor())  == -1) { // atual < paiAtual
+                        paiAtual.setEsquerda(null);
+
+                    } else {
+                        paiAtual.setDireita(null);
+
+                    }
+                } else { // É a raiz.
+                    this.raiz = null;
+                }
+            }
+
+            return true;
+        } else {
+            return false;
         }
     }
 }
--- ArvoreBinaria/Main.java
+++ ArvoreBinaria/Main.java
@@ -9,14 +9,58 @@ public class Main {
         arvore.adicionar(18);
         arvore.adicionar(13);
         arvore.adicionar(20);
+
         System.out.println("\n\nÁrvore em ordem:");
         arvore.emOrdem(arvore.getRaiz());
 
-        System.out.println("\n\nÁrvore em pré-ordem:");
-        arvore.preOrdem(arvore.getRaiz());
+        arvore.remover(20);
 
-        System.out.println("\n\nÁrvore em pós-ordem:");
-        arvore.posOrdem(arvore.getRaiz());
+        System.out.println("\n\nÁrvore em ordem:");
+        arvore.emOrdem(arvore.getRaiz());
+
+        arvore.remover(5);
+
+        System.out.println("\n\nÁrvore em ordem:");
+        arvore.emOrdem(arvore.getRaiz());
+
+        arvore.remover(8);
+        
+        System.out.println("\n\nÁrvore em ordem:");
+        arvore.emOrdem(arvore.getRaiz());
+
+        arvore.remover(9);
+
+        System.out.println("\n\nÁrvore em ordem:");
+        arvore.emOrdem(arvore.getRaiz());
+
+        arvore.remover(13);
+
+        System.out.println("\n\nÁrvore em ordem:");
+        arvore.emOrdem(arvore.getRaiz());
+
+        arvore.remover(7);
+
+        System.out.println("\n\nÁrvore em ordem:");
+        arvore.emOrdem(arvore.getRaiz());
+        
+        arvore.remover(18);
+
+        System.out.println("\n\nÁrvore em ordem:");
+        arvore.emOrdem(arvore.getRaiz());
+        
+        arvore.remover(10);
+
+        System.out.println("\n\nÁrvore em ordem:");
+        arvore.emOrdem(arvore.getRaiz());
+
+
+        // System.out.println("\n\nÁrvore em pré-ordem:");
+        // arvore.preOrdem(arvore.getRaiz());
+
+        // System.out.println("\n\nÁrvore em pós-ordem:");
+        // arvore.posOrdem(arvore.getRaiz());
+
+
     }
     
 }
